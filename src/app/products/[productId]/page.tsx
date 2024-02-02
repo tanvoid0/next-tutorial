353cb4a2ab4a1c6be2bda096c0ddd63ff214cc43
Tanveer <tanvoid0@gmail.com>
2024-02-02 08:02:23 +0000

    17.2 Routing metadata with Promise

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -6,9 +6,14 @@ type Props = {
     }
 }
 
-export const generateMetadata = ({params}: Props): Metadata => {
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
     return {
-        title: `Product ${params.productId}`
+        title: `Product ${title}`
     }
 }
 
