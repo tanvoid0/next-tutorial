61157afcd172e0adc558368da98fffe5d7297538
Tanveer <tanvoid0@gmail.com>
2024-02-02 07:53:50 +0000

    16. Route Group Layout

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+	                                             children,
+                                             }: {
+	children: React.ReactNode;
+}) {
+	return (
+		<>
+			<h2>Inner layout
+			</h2>
+			{children}
+		</>
+	)
+}
