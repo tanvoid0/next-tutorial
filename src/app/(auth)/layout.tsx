bf2310fe1e8ebee7b6f4cd0fba608023e772ba22
Tanveer <tanvoid0@gmail.com>
2024-02-03 00:29:46 +0000

    21. Navigating Programmatically

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -21,7 +21,7 @@ export default function AuthLayout({
 				navLinks.map((link) => {
 					const isActive = pathname.startsWith(link.href);
 					return (
-						<Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4": "text-blue-500 mr-4"}>
+						<Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
 							{link.name}
 						</Link>
 					)
