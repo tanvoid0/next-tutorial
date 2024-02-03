7d66a58199aa724d8a0d6da39f45385ea28e7360
Tanveer <tanvoid0@gmail.com>
2024-02-03 13:29:56 +0000

    24 - Error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,18 @@
 import {notFound} from "next/navigation";
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
 export default function ReviewDetail({params}: {
     params: {
         productId: string,
         reviewId: string,
     }
 }) {
+    const random =  getRandomInt(2);
+    if (random === 1) {
+        throw new Error("Error Loading review");
+    }
     if (parseInt(params.reviewId) > 1000) {
         notFound();
     }
