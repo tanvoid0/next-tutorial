6d75b01a66cba7b1ed30686fa5c9845be1ed2a39
Tanveer <tanvoid0@gmail.com>
2024-02-04 20:27:37 +0000

    42. Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,21 @@
 import {type NextRequest} from "next/server";
-import {headers} from "next/headers";
+import {cookies, headers} from "next/headers";
 
 export async function GET(request: NextRequest) {
 	// const requestHeaders = new Headers(request.headers);
 	const requestHeaders = headers();
+
+	cookies().set("resultsPerPage", "20");
+	const theme = request.cookies.get("theme");
+
 	console.log(requestHeaders.get("Authorization"));
+
+	console.log(theme);
+	console.log(cookies().getAll());
 	return new Response("<h1>Profile API data</h1>", {
 		headers: {
-			"Content-Type": 'text/html'
+			"Content-Type": 'text/html',
+			"Set-Cookie": "theme=dark"
 		}
 	});
 }
