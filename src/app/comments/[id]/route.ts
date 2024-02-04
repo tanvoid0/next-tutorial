81a86f3acdb2da6fac50a40345ad4a23729de719
Tanveer <tanvoid0@gmail.com>
2024-02-04 20:01:11 +0000

    37. Handling PATCH Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,6 +1,29 @@
 import {comments} from "@/app/comments/data";
 
 export async function GET(request: Request, {params}: { params: { id: string } }) {
-	const comment = comments.find(comment => comment.id == parseInt(params.id));
+	const comment = comments.find(comment => comment.id === parseInt(params.id));
 	return Response.json(comment);
+}
+
+export async function PATCH(request: Request, {params}: { params: { id: string } }) {
+	const body = await request.json();
+	const {text} = body;
+	const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+	if (index === -1) {
+		return Response.json(
+			{
+				message: `Comment with id ${params.id} not found`
+			},
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+				status: 404,
+			}
+		)
+	}
+	comments[index].text = text;
+	return Response.json(
+		comments[index]
+	)
 }
